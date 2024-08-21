94d5d68e979d88765efe8bdb95f74f9cc20f78bf
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-21 08:12:58 -0300

    style: Implement Dynamic Style

--- src/components/Modals/CreatePerson/Body/index.tsx
+++ src/components/Modals/CreatePerson/Body/index.tsx
@@ -80,16 +80,23 @@ export default function CreatePersonBody(props: info) {
     const avatars = getAllAvatar();
 
     const setAvatarImage = (selected: number) => {
+        styleElement()
         let selectedAvatar = avatars[selected]
         setAvatar(selectedAvatar)
         handleAvatar(selected+1)
     }
 
+    const styleElement = () => {
+        let element = document.getElementById("avatar")
+        element?.classList.add(styles.imageDefined)
+
+    }
+    
     return (
         <>
             <ModalBody>
                 <Flex className={styles.avatarWrapper}>
-                    <Image className={styles.avatarImg} src={avatar}/>
+                    <Image id="avatar" className={styles.avatarImg} src={avatar}/>
                     <Text className={styles.avatarTitle}>Your Avatar</Text>
                     <Flex className={styles.optionsWrapper}>
                         {avatars.map((avatar, index) => (
@@ -97,24 +104,26 @@ export default function CreatePersonBody(props: info) {
                         ))}
                     </Flex>
                 </Flex>
-                <Box>
-                    <FormControl>
+                <Box display={"flex"}>
+                    <FormControl mt={4}>
                         <FormLabel>First Name:</FormLabel>
                         <Input placeholder='First name' name='firstName' value={formData.firstName} onChange={handleChange}/>
                     </FormControl>
-                </Box>
-                <FormControl mt={4}>
-                    <FormLabel>Last Name:</FormLabel>
-                    <Input placeholder='Last name' name='lastName' value={formData.lastName} onChange={handleChange}/>
-                </FormControl>
-                <FormControl mt={4}>
-                    <FormLabel>CPF:</FormLabel>
-                    <Input className={styles.inputs} placeholder='123.456.789-00' as={InputMask} mask={"999.999.999-99"} maskChar={null} name='cpf' value={formData.cpf} onChange={handleChange}/>
-                </FormControl>
                     <FormControl mt={4}>
-                    <FormLabel>Birth Date:</FormLabel>
-                    <Input placeholder='12/05/1997' type='date' name='birthDate' value={formData.birthDate} onChange={handleChange}/>
-                </FormControl>
+                        <FormLabel>Last Name:</FormLabel>
+                        <Input placeholder='Last name' name='lastName' value={formData.lastName} onChange={handleChange}/>
+                    </FormControl>
+                </Box>
+                <Box display={"flex"}>
+                    <FormControl mt={4}>
+                        <FormLabel>CPF:</FormLabel>
+                        <Input className={styles.inputs} placeholder='123.456.789-00' as={InputMask} mask={"999.999.999-99"} maskChar={null} name='cpf' value={formData.cpf} onChange={handleChange}/>
+                    </FormControl>
+                        <FormControl mt={4}>
+                        <FormLabel>Birth Date:</FormLabel>
+                        <Input placeholder='12/05/1997' type='date' name='birthDate' value={formData.birthDate} onChange={handleChange}/>
+                    </FormControl>
+                </Box>
             </ModalBody>
             <ModalFooter justifyContent={"center"}>
                 <Button color='white' bg={"main.100"} onClick={handleSubmit}>
